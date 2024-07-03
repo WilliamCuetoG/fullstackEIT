6f760aa7ee8218ea566ebaa4937e1c6ec3716bf0
William <williamcg834@gmail.com>
2024-07-03 13:09:08 -0300

    Inicio mi Proyecto Final

---
+++ login.js
@@ -0,0 +1,13 @@
+
+let email = 'admin@gmail.com';
+let password = '1234';
+
+
+
+
+// Recibimos datos del user
+let emailUser = prompt('Introduce tu Email');
+let passwordUser = prompt('Introduce tu Password');
+
+//Comparar los datos del user con la databse
+if(email )
